d7a8411e1030754f3693e7df85d3ccdcf7ccec4e
Theo <18117435+theojlab@users.noreply.github.com>
2020-03-24 13:47:12 -0400

    prevent elephant image from scaling

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -9,24 +9,37 @@
 
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
-
+<style>
+    .top-text {
+        padding-left: 1em;
+    }
+</style>
 </head>
 
 <div id="app">
 <div class="container-fluid">
     <div class="row">
             <div class="col-md-12">
-                    <b-card img-src="{{asset('img/myaElephant.png')}}" img-left class="mb-3">
-                        <h2>
-                            Core Archive Set Request Form
-                        </h2>
-                        <p>
-                            Use the forms below to request adding channels to the Mya archiver or to change deadbands or metadata of
-                            currently archived channels.  For other types of requests, please <a href="#">contact the Mya Administrator</a>.
-                        </p>
-                        <p>
-                            <a class="btn btn-primary btn-large" href="#">Learn more</a>
-                        </p>
+                    <b-card img-width="135px">
+                        <b-row>
+                            <b-col md="2">
+                                <img width="135px" src="{{asset('img/myaElephant.png')}}"></img>
+                            </b-col>
+                            <b-col md="10">
+                                <div class="top-text">
+                                <h2>
+                                    Core Archive Set Request Form
+                                </h2>
+                                <p>
+                                    Use the forms below to request adding channels to the Mya archiver or to change deadbands or metadata of
+                                    currently archived channels.  For other types of requests, please <a href="#">contact the Mya Administrator</a>.
+                                </p>
+                                <p>
+                                    <a class="btn btn-primary btn-large" href="#">Learn more</a>
+                                </p>
+                                </div>
+                            </b-col>
+                        </b-row>
                     </b-card>
                 </div>
     </div>
